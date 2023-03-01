0d847e3b223e801a8237cebd1e5f3f1e8668f615
Minhaz-Khan <kminhaz111@gmail.com>
2023-03-02 01:10:24 +0600

    vercel deploy done

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const app = express();
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
+require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
 
-// userName: prosPlum
-// password : lSqfKkMOoekD9BiX
 
-const uri = "mongodb+srv://prosPlum:lSqfKkMOoekD9BiX@cluster0.drtwsrz.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.REACT_APP_DBUSER}:${process.env.REACT_APP_DBPASSWORD}@cluster0.drtwsrz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
@@ -34,18 +33,18 @@ async function run() {
         app.put('/review/:id', async (req, res) => {
             const id = req.params.id;
             const review = req.body.feedback;
-            const filter = { _id: ObjectId(id) };
-            const upDateDoc = {
-                $ser: {
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set:{
                     feedback: review,
-                }
+                },
             }
-            const result = await reviewCollection.updateOne(filter, upDateDoc);
+            const result = await reviewCollection.updateOne(filter, updateDoc);
             res.send(result);
         })
         app.delete('/review/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectId(id) };
+            const query = { _id: new ObjectId(id) };
             const result = await reviewCollection.deleteOne(query);
             res.send(result);
         })
@@ -71,6 +70,20 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await serviceCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.get('/myService',async(req,res)=>{
+            const userEmail = req.query.email;
+            const query = {email:userEmail}
+            const myService =  serviceCollection.find(query)
+            const result = await myService.toArray();
+            res.send(result)
+        })
+        app.delete('/myService/:id',async (req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await serviceCollection.deleteOne(query);
             res.send(result);
         })
 
